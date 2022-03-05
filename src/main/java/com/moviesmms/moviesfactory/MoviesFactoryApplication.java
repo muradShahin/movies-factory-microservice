e9746528ba2f70a3afcd6c5ade4727812ee0f9bd
muradshahin <shahinmurad43@gmail.com>
2022-03-05 16:41:07 +0300

    removing DS configurations

--- src/main/java/com/moviesmms/moviesfactory/MoviesFactoryApplication.java
+++ src/main/java/com/moviesmms/moviesfactory/MoviesFactoryApplication.java
@@ -9,9 +9,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MoviesFactoryApplication  implements CommandLineRunner{
-	
-	@Autowired
-	JpaEntity repository;
+//	
+//	@Autowired
+//	JpaEntity repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoviesFactoryApplication.class, args);
@@ -20,12 +20,12 @@ public class MoviesFactoryApplication  implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		
-		long count = repository.count();
-
-		if (count == 0) {
-			repository.save(new Movie(1001,"Spider Man 2002","http://image","the Begining of spider",BigDecimal.valueOf(7)));
-			
-		}
+//		long count = repository.count();
+//
+//		if (count == 0) {
+//			repository.save(new Movie(1001,"Spider Man 2002","http://image","the Begining of spider",BigDecimal.valueOf(7)));
+//			
+//		}
 	}
 
 }
